damurashov/microtojson

commit b0e0a954934cf50b09a8cf79407347edfa4caec8

Add Wconversion

mtojson.c
@@ -9,8 +9,17 @@
 #include <stdio.h>
 #include <string.h>
 
-static int rem_len;
+static size_t rem_len;
 static int nested_object_depth = 0;
+
+static int
+reduce_rem_len(size_t len)
+{
+	if (rem_len < len)
+		return 0;
+	rem_len -= len;
+	return 1;
+}
 
 static char*
 gen_json_boolean(char *out, _Bool val)
@@ -22,8 +31,9 @@ gen_json_boolean(char *out, _Bool val)
 		v = t;
 	else
 		v = f;
-	rem_len -= strlen(v);
-	if (rem_len < 0) return NULL;
+	size_t len = strlen(v);
+	if (!reduce_rem_len(len))
+		return NULL;
 	while ((*out++ = *v++));
 	--out; // Discard \0
 	return out;
@@ -32,8 +42,8 @@ gen_json_boolean(char *out, _Bool val)
 static char*
 gen_json_string(char *out, char *val)
 {
-	rem_len -= (strlen(val) + 2); // ""
-	if (rem_len < 0)
+	size_t len = strlen(val);
+	if (!reduce_rem_len(len + 2)) // ""
 		return NULL;
 	*out++ = '"';
 	while ((*out++ = *val++));
@@ -48,8 +58,8 @@ gen_json_integer(char *out, int val)
 	#define INT_STRING_SIZE ((sizeof(int)*CHAR_BIT - 1)*28/93 + 3)
 	char buf[INT_STRING_SIZE];
 	sprintf(buf, "%d", val);
-	rem_len -= strlen(buf);
-	if (rem_len < 0)
+	size_t len = strlen(buf);
+	if (!reduce_rem_len(len + 0))
 		return NULL;
 	char *ptr = &buf[0];
 	while ((*out++ = *ptr++));
@@ -61,8 +71,8 @@ gen_json_integer(char *out, int val)
 static char*
 gen_json_value(char *out, char *val)
 {
-	rem_len -= strlen(val);
-	if (rem_len < 0)
+	size_t len = strlen(val);
+	if (!reduce_rem_len(len + 0))
 		return NULL;
 	while ((*out++ = *val++));
 	--out; // Discard \0
@@ -72,7 +82,9 @@ gen_json_value(char *out, char *val)
 static char*
 gen_json_array(char *out, struct json_array *jar)
 {
-	if ((rem_len -= 2) < 0) return NULL; // 2 -> []
+	if (!reduce_rem_len(2)) // 2 -> []
+		return NULL;
+
 	*out++ = '[';
 	if (jar->count == 0){
 		*out++ = ']';
@@ -135,21 +147,26 @@ gen_json_array(char *out, struct json_array *jar)
 }
 
 char*
-generate_json(char *out, int len, struct json_kv *kv)
+generate_json(char *out, size_t len, struct json_kv *kv)
 {
-	int object_meta_len = 2; // 2 -> {}
+	rem_len = len;
+	size_t object_meta_len = 2; // 2 -> {}
 	if (nested_object_depth == 0)
 		object_meta_len = 3; // 3 -> {}\0
 	nested_object_depth++;
-	if((rem_len = len - object_meta_len) < 0) goto fail;
+
+	if (!reduce_rem_len(object_meta_len))
+		goto fail;
+
 	*out++ = '{';
 	if (!kv->key)
 		goto done;
 
 	while (kv->key){
 		char *key = kv->key;
-		rem_len -= (strlen(key) + 4); // 4 -> "":_
-		if (rem_len < 0) goto fail;
+		size_t l = strlen(key);
+		if (!reduce_rem_len(l + 4)) // 4 -> "":_
+			goto fail;
 
 		*out++ = '"';
 		while ((*out++ = *key++));
@@ -181,14 +198,15 @@ generate_json(char *out, int len, struct json_kv *kv)
 
 		if (!out)
 			goto fail;
-		rem_len -= 2;
-		*out++ = ',';
-		*out++ = ' ';
+
+		if ((kv + 1)->key){
+			rem_len -= 2;
+			*out++ = ',';
+			*out++ = ' ';
+		}
+
 		kv++;
 	}
-
-	rem_len += 2;
-	out -= 2;
 
 done:
 	*out++ = '}';

Makefile
@@ -2,7 +2,7 @@ CC = gcc
 
 CFLAGS = -std=c99
 CFLAGS += -Wall -Wextra -Wpedantic
-CFLAGS += -fno-common -Wshadow
+CFLAGS += -fno-common -Wshadow -Wconversion
 CFLAGS += -g -O2
 
 ASAN = -fsanitize=address,undefined -fno-omit-frame-pointer

mtojson.h
@@ -2,6 +2,7 @@
 #define RKTA_MTOJSON_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 enum json_value_type {
 	t_to_array,
@@ -24,5 +25,5 @@ struct json_array {
 	enum json_value_type type;
 };
 
-char* generate_json(char *out, int len, struct json_kv *kvs);
+char* generate_json(char *out, size_t len, struct json_kv *kvs);
 #endif

test_mtojson.c
@@ -56,7 +56,7 @@ test_json_string()
 {
 	char *expected = "{\"key\": \"value\"}";
 	char *test = "test_json_string";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -76,7 +76,7 @@ test_json_boolean()
 {
 	char *expected = "{\"key\": true}";
 	char *test = "test_json_boolean";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -97,7 +97,7 @@ test_json_integer()
 {
 	char *expected = "{\"key\": 1}";
 	char *test = "test_json_integer";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -118,7 +118,7 @@ test_json_integer_two()
 {
 	char *expected = "{\"key\": 1, \"key\": 2}";
 	char *test = "test_json_integer_two";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -141,7 +141,7 @@ test_json_array_integer()
 {
 	char *expected = "{\"array\": [1, 2]}";
 	char *test = "test_json_array_integer";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -165,7 +165,7 @@ test_json_array_string()
 {
 	char *expected = "{\"array\": [\"1\", \"23\"]}";
 	char *test = "test_json_array_string";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -189,7 +189,7 @@ test_json_array_boolean()
 {
 	char *expected = "{\"array\": [true, false]}";
 	char *test = "test_json_array_boolean";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -213,7 +213,7 @@ test_json_array_array()
 {
 	char *expected = "{\"array\": [[\"1\", \"2\", \"3\"], [\"1\", \"2\", \"3\"]]}";
 	char *test = "test_json_array_array";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -241,7 +241,7 @@ test_json_array_empty()
 {
 	char *expected = "{\"array\": []}";
 	char *test = "test_json_array_empty";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -266,7 +266,7 @@ test_json_array_empty_one()
 {
 	char *expected = "{\"array\": [[], [\"1\", \"2\", \"3\"]]}";
 	char *test = "test_json_array_one_empty";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -297,7 +297,7 @@ test_json_object_empty()
 {
 	char *expected = "{}";
 	char *test = "test_json_object_empty";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -324,7 +324,7 @@ test_json_object()
 	                 "}";
 
 	char *test = "test_json_object";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -372,7 +372,7 @@ test_json_array_object()
 	                 "}";
 
 	char *test = "test_json_array_object";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -431,7 +431,7 @@ test_json_object_object()
 	                 "}";
 
 	char *test = "test_json_object_object";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -471,7 +471,7 @@ test_json_object_nested_empty()
 	                 "}";
 
 	char *test = "test_json_object_nested_empty";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;
@@ -506,7 +506,7 @@ test_json_valuetype()
 {
 	char *expected = "{\"key\": This is not valid {}JSON!}";
 	char *test = "test_json_valuetype";
-	int len = strlen(expected) + 1;
+	size_t len = strlen(expected) + 1;
 	char result[len];
 	memset(result, '\0', len);
 	rp = result;

commit 69137a15e88957f1288a8a94e2a3e3b3c1f07eae

Fix potential stack overflow
This regression was introduced with

commit b0e0a954934cf50b09a8cf79407347edfa4caec8
Author: Rene Kita <git@rkta.de>
Date:   2020-12-29T20:58:57+01:00

    Add Wconversion

 4 files changed, 58 insertions(+), 39 deletions(-)

mtojson.c
@@ -133,7 +133,8 @@ gen_array_type(char *out, const void *val, _Bool is_last, char* (*func)())
 	if (!out)
 		return NULL;
 	if (!is_last){
-		rem_len -= 2;
+		if (!reduce_rem_len(2))
+			return NULL;
 		*out++ = ',';
 		*out++ = ' ';
 	}
@@ -243,7 +244,8 @@ gen_object(char *out, const struct json_kv *kv)
 			goto fail;
 
 		if ((kv + 1)->key){
-			rem_len -= 2;
+			if (!reduce_rem_len(2))
+				goto fail;
 			*out++ = ',';
 			*out++ = ' ';
 		}